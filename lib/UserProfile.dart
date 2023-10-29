becf4ac0325d65a5f355c084f6640bf67b92ea8e
rickNrip <117496632+rickNrip@users.noreply.github.com>
2023-10-28 21:59:36 -0700

    FixesGoalWeight

--- lib/UserProfile.dart
+++ lib/UserProfile.dart
@@ -22,6 +22,21 @@ class _UserProfilePageState extends State<UserProfilePage> {
 
   List<String> allergies = [];
   List<String> healthIssues = [];
+  // Add age controller for the BMR calculation
+  final TextEditingController _ageController = TextEditingController();
+
+  // This function returns the estimated TDEE
+  double calculateTDEE(double weight, double heightInInches, int age) {
+    double heightInCm = heightInInches * 2.54;
+    double weightInKg = weight * 0.453592; // convert lbs to kg
+
+    // Mifflin-St Jeor Equation for men as an example.
+    // Adjust this for women or other genders as necessary
+    double bmr = (10 * weightInKg) + (6.25 * heightInCm) - (5 * age) + 5;
+    double tdee = bmr * 1.2; // Assuming sedentary activity level
+
+    return tdee;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +57,15 @@ class _UserProfilePageState extends State<UserProfilePage> {
               ),
             ),
             SizedBox(height: 16),
+            // Add Age TextField somewhere appropriate in your form
+            TextField(
+              controller: _ageController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Age',
+                border: OutlineInputBorder(),
+              ),
+            ),
             TextField(
               controller: _heightFeetController,
               keyboardType: TextInputType.number,
@@ -120,6 +144,23 @@ class _UserProfilePageState extends State<UserProfilePage> {
                 double heightInInches =
                     (double.tryParse(_heightFeetController.text) ?? 0) * 12 +
                         (double.tryParse(_heightInchesController.text) ?? 0);
+                double weight = double.tryParse(_weightController.text) ?? 0;
+                int age = int.tryParse(_ageController.text) ?? 0;
+
+                double tdee = calculateTDEE(weight, heightInInches, age);
+                double goalcals = tdee - 500;
+
+                double goalprotein = goalcals * 0.15 / 4;
+                double goalcarbs = goalcals * 0.55 / 4;
+                double goalfat = goalcals * 0.30 / 9;
+                double goalsugar = goalcals * 0.05 / 4;
+                double goalsodium = 2300.0;
+
+                // Rounding to nearest hundredth
+                goalcals = (goalcals * 100).roundToDouble() / 100;
+                goalprotein = (goalprotein * 100).roundToDouble() / 100;
+                goalsugar = (goalsugar * 100).roundToDouble() / 100;
+                goalfat = (goalfat * 100).roundToDouble() / 100;
 
                 User? currentUser = _auth.currentUser;
                 if (currentUser != null) {
@@ -127,18 +168,23 @@ class _UserProfilePageState extends State<UserProfilePage> {
                       .collection('users')
                       .doc(currentUser.uid)
                       .update({
-                    'weight': _weightController.text,
+                    'weight': weight,
                     'height': heightInInches,
                     'goalWeight': _goalWeightController.text,
                     'allergies': allergies,
                     'healthIssues': healthIssues,
+                    'goalcals': goalcals,
+                    'goalprotein': goalprotein,
+                    'goalsugar': goalsugar,
+                    'goalfat': goalfat,
+                    'goalsodium': goalsodium,
                   });
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Profile updated!')),
+                    SnackBar(content: Text('Profile and goals updated!')),
                   );
                 }
               },
-              child: Text('Update Profile'),
+              child: Text('Update Profile and Goals'),
             ),
           ],
         ),
@@ -148,6 +194,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
 
   @override
   void dispose() {
+    _ageController.dispose();
     _weightController.dispose();
     _heightFeetController.dispose();
     _heightInchesController.dispose();
